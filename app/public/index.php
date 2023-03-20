172a36ae698f4db75fb20e53e379bc827c20579b
Mehdi H <78570781+MH-DevApp@users.noreply.github.com>
2023-03-20 23:40:47 +0100

    feat #5 : init composer, add autoload in index.php and install some dependencies : (#38)
     - PHPStan ;
     - PHPCS ;
     - PHPUnit with config file ;
     - PHPUnit code coverage.

---
+++ app/public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Application startup file
+ *
+ * PHP Version 8.1
+ *
+ * @category PHP
+ * @package  OC_P5_BLOG
+ * @author   Mehdi Haddou <mehdih.devapp@gmail.com>
+ * @license  MIT Licence
+ * @link     https://p5.mehdi-haddou.fr
+ */
+
+declare(strict_types=1);
+
+require_once "../vendor/autoload.php";
