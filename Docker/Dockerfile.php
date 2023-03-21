95649c897aef616860b60fabf83956206905cc01
Mehdi H <78570781+MH-DevApp@users.noreply.github.com>
2023-03-21 21:45:19 +0100

    feat #1 : Set up a docker environment dev : (#39)
    - PHP Container : running on port 3000 in host and 8000 in network docker ;
    - MySQL : running on port 3001 in host and 3306 in network docker ;
    - PhpMyAdmin : running on port 8080 in host and 80 in network docker ;
    - MailCatcher : running on port 5000 & 5001 in host and 1025 & 1080 in network docker.

---
+++ Docker/Dockerfile.php
@@ -0,0 +1,11 @@
+FROM php:latest
+RUN mv "$PHP_INI_DIR/php.ini-development" "$PHP_INI_DIR/php.ini"
+RUN curl -sS https://getcomposer.org/installer | php -- --filename=composer --install-dir=/usr/local/bin
+RUN apt update && apt install -y zip git libicu-dev locales
+RUN docker-php-ext-install opcache intl pdo_mysql
+RUN locale-gen fr_FR.UTF-8
+WORKDIR /app
+COPY ./app/composer.json .
+RUN composer install
+COPY ./app .
+CMD ["php", "-S", "0.0.0.0:8000", "-t", "public/"]
