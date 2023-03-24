010a9f8480d37aeef006ff28b8937bb0348f6565
Mehdi H <78570781+MH-DevApp@users.noreply.github.com>
2023-03-24 23:55:08 +0100

    feat (#50) : Route class (#10) and Tests (#11)

---
+++ app/src/Factory/Router/Route.php
@@ -0,0 +1,174 @@
+<?php
+
+/**
+ * Route file
+ *
+ * PHP Version 8.1
+ *
+ * @category PHP
+ * @package  OC_P5_BLOG
+ * @author   Mehdi Haddou <mehdih.devapp@gmail.com>
+ * @license  MIT Licence
+ * @link     https://p5.mehdi-haddou.fr
+ */
+
+declare(strict_types=1);
+
+namespace App\Factory\Router;
+
+/**
+ * Response class
+ * Returns a response text with Header and status-code
+ * to the client
+ *
+ * PHP Version 8.1
+ *
+ * @category PHP
+ * @package  OC_P5_BLOG
+ * @author   Mehdi Haddou <mehdih.devapp@gmail.com>
+ * @license  MIT Licence
+ * @link     https://p5.mehdi-haddou.fr
+ */
+class Route
+{
+
+    /**
+     * @var array<int, string|int> $params
+     */
+    private array $params = [];
+
+
+    /**
+     * Construct
+     *
+     * @param string                $path    Path route (for example /posts or
+                                       /posts/:id with params)
+     * @param string                $name    Name of route (for example app_posts)
+     * @param array<string, string> $regexs  [Optional] array of regexs for params
+     * @param array<int, string>    $methods [Optional] array of methods, by default GET
+     */
+    public function __construct(
+        private string $path,
+        private readonly string $name,
+        private array $regexs=[],
+        private readonly array $methods=["GET"]
+    ) {
+        $this->path = trim($this->path, "/");
+
+        // Replace to remove bracket open.
+        foreach ($this->regexs as $key => $value) {
+            $this->regexs[$key] = str_replace('(', '(?:', $value);
+        }
+
+    }
+
+
+    /**
+     * Math all params between URL and path.
+     * If match then return true else false.
+     *
+     * @param string $url This URI of the request
+     *
+     * @return bool
+     */
+    public function match(string $url): bool
+    {
+        $url = trim($url, '/');
+        $path = preg_replace_callback(
+            '#:(\w+)#',
+            [
+             $this,
+             'paramMatch'
+            ],
+            $this->path
+        );
+        $regex = "#^$path$#i";
+
+        /*
+         * Return false if no match with regex path else
+         * get all params of the url matches with regex path.
+         */
+
+        if (preg_match($regex, $url, $matches) !== PREG_SPLIT_NO_EMPTY) {
+            return false;
+        }
+
+        // Remove first item and keep matches word.
+        array_shift($matches);
+        $this->params = $matches;
+
+        return true;
+
+    }
+
+
+    /**
+     * Get regex if defined else return default
+     * regex value
+     *
+     * @param array<int, string> $match Param in path
+     *
+     * @return string
+     */
+    private function paramMatch(array $match): string
+    {
+        if (isset($this->regexs[$match[1]]) === true) {
+            return '('.$this->regexs[$match[1]].')';
+        }
+
+        // Return default regex value.
+        return '([^/]+)';
+
+    }
+
+
+    /**
+     * Get path of the route
+     *
+     * @return string
+     */
+    public function getPath(): string
+    {
+        return $this->path;
+
+    }
+
+
+    /**
+     * Get name of the route
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+
+    }
+
+
+    /**
+     * Get methods of the route
+     * GET, POST
+     *
+     * @return array<int, string>
+     */
+    public function getMethods(): array
+    {
+        return $this->methods;
+
+    }
+
+
+    /**
+     * Get params of the route
+     *
+     * @return array<int, string|int>
+     */
+    public function getParams(): array
+    {
+        return $this->params;
+
+    }
+
+
+}
---
+++ app/tests/Factory/Router/RouterRouteTest.php
@@ -0,0 +1,123 @@
+<?php
+
+/**
+ * RouterRouteTest file
+ *
+ * PHP Version 8.1
+ *
+ * @category PHP
+ * @package  OC_P5_BLOG
+ * @author   Mehdi Haddou <mehdih.devapp@gmail.com>
+ * @license  MIT Licence
+ * @link     https://p5.mehdi-haddou.fr
+ */
+
+declare(strict_types=1);
+
+namespace tests\Factory\Router;
+
+
+use App\Factory\Router\Route;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestDox;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test Router Route cases
+ *
+ * PHP Version 8.1
+ *
+ * @category PHP
+ * @package  OC_P5_BLOG
+ * @author   Mehdi Haddou <mehdih.devapp@gmail.com>
+ * @license  MIT Licence
+ * @link     https://p5.mehdi-haddou.fr
+ */
+#[CoversClass(Route::class)]
+class RouterRouteTest extends TestCase
+{
+
+
+    /**
+     * Test should be get match simple route.
+     *
+     * @return void
+     */
+    #[Test]
+    #[TestDox("should be get match simple route")]
+    public function itMatchSimpleRoute(): void
+    {
+        $route = new Route(
+            "/test",
+            "app_test"
+        );
+
+        $this->assertTrue($route->match("/test"));
+        $this->assertEquals("test", $route->getPath());
+        $this->assertEquals(["GET"], $route->getMethods());
+        $this->assertEquals("app_test", $route->getName());
+        $this->assertEquals([], $route->getParams());
+
+    }
+
+
+    /**
+     * Test should be get match complex route with default
+     * value regex.
+     *
+     * @return void
+     */
+    #[Test]
+    #[TestDox("should be get match complex route with default value regex")]
+    public function itMatchComplexRouteWithDefaultRegex(): void
+    {
+        $route = new Route(
+            "/test/:id/:slug",
+            "app_test_id_and_slug"
+        );
+
+        $this->assertTrue($route->match("/test/1/hello"));
+        $this->assertFalse($route->match("/test/1"));
+        $this->assertEquals("test/:id/:slug", $route->getPath());
+        $this->assertEquals(["GET"], $route->getMethods());
+        $this->assertEquals("app_test_id_and_slug", $route->getName());
+        $this->assertEquals(["1", "hello"], $route->getParams());
+
+    }
+
+
+    /**
+     * Test should be get match complex route with custom
+     * value regex.
+     *
+     * @return void
+     */
+    #[Test]
+    #[TestDox("should be get match complex route with custom value regex")]
+    public function itMatchComplexRouteWithCustomRegex(): void
+    {
+        $route = new Route(
+            "/test/:id/:slug",
+            "app_test_id_and_slug",
+            [
+             "id" => "[0-9]+",
+             "slug" => "([a-z\-0-9])+"
+            ],
+            [
+             "GET",
+             "POST"
+            ],
+        );
+
+        $this->assertTrue($route->match("/test/1/hello"));
+        $this->assertFalse($route->match("/test/hello/1"));
+        $this->assertEquals("test/:id/:slug", $route->getPath());
+        $this->assertEquals(["GET", "POST"], $route->getMethods());
+        $this->assertEquals("app_test_id_and_slug", $route->getName());
+        $this->assertEquals(["1", "hello"], $route->getParams());
+
+    }
+
+
+}
