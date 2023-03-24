f078167e0476b0a8f0870f578b03f60a7e4a77cd
Mehdi H <78570781+MH-DevApp@users.noreply.github.com>
2023-03-24 18:52:35 +0100

    Feature (#47) : Response class #8 and Tests #9

---
+++ app/src/Factory/Router/Response.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Response file
+ *
+ * PHP Version 8.1
+ *
+ * @category PHP
+ * @package  OC_P5_BLOG
+ * @author   Mehdi Haddou <mehdih.devapp@gmail.com>
+ * @license  MIT Licence
+ * @link     https://p5.mehdi-haddou.fr
+ */
+
+declare(strict_types=1);
+
+namespace App\Factory\Router;
+
+/**
+ * Response class
+ * Returns a response text with Header and status-code
+ * to the client
+ *
+ * PHP Version 8.1
+ *
+ * @category PHP
+ * @package  OC_P5_BLOG
+ * @author   Mehdi Haddou <mehdih.devapp@gmail.com>
+ * @license  MIT Licence
+ * @link     https://p5.mehdi-haddou.fr
+ */
+class Response
+{
+
+
+    /**
+     * Construct
+     *
+     * @param string             $content [Optional] Text to render
+     * @param int                $status  [Optional] Status code to response
+     * @param array<int, string> $headers [Optional] headers to response
+     */
+    public function __construct(
+        private readonly string $content="",
+        private readonly int $status=200,
+        private readonly array $headers=['Content-Type: text/html; charset=utf-8']
+    ) {
+        http_response_code($this->status);
+
+    }
+
+
+    /**
+     * Function send test to render client
+     *
+     * @return void
+     */
+    public function send(): void
+    {
+        foreach ($this->headers as $header) {
+            header($header);
+        }
+
+        echo htmlspecialchars($this->content, ENT_QUOTES, "UTF-8");
+
+    }
+
+
+}
