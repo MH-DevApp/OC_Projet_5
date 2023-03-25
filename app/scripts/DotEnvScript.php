4a5e91b4a29ef08195f75b293706d38f25352cca
Mehdi H <78570781+MH-DevApp@users.noreply.github.com>
2023-03-25 18:09:57 +0100

    fix (#57) : fix errors of SymfonyInsight (#56)
    - add new line character to the end of files services.yml and .gitignore
    - update composer
    - remove function sleep in DotEnvScript file
    - add files : favicon.ico, robots.txt and site.webmanifest in public folder

--- app/scripts/DotEnvScript.php
+++ app/scripts/DotEnvScript.php
@@ -23,18 +23,15 @@ $default_data .= "DB_DNS=mysql:host=localhost:3306;dbname=db_name".PHP_EOL;
 $default_data .= "DB_USER=root".PHP_EOL;
 $default_data .= "DB_PWD=YourPassword";
 file_put_contents(__DIR__."/../.env", $default_data, LOCK_EX);
-sleep(1);
 echo "# File .env created : it is necessary to modify the default values 
     with your data.".PHP_EOL.PHP_EOL;
 
 echo "# Creation of the '.env_test' file in the root of the app folder."
     .PHP_EOL.PHP_EOL;
-sleep(1);
 $default_data =  "APP_ENV=TEST".PHP_EOL;
 $default_data .= "DB_DNS=mysql:host=localhost:3306;dbname=db_name_test".PHP_EOL;
 $default_data .= "DB_USER=root".PHP_EOL;
 $default_data .= "DB_PWD=YourPassword";
 file_put_contents(__DIR__."/../.env_test", $default_data, LOCK_EX);
-sleep(1);
 echo "# File .env_test created : it is necessary to modify the default values 
     with your data.".PHP_EOL.PHP_EOL;
