56a966ddd2d2f268095fcf847700b3fe10c68b96
mehdi <mehdih.devapp@gmail.com>
2023-03-25 16:55:14 +0100

    feat : add Container class (#12)

---
+++ app/src/Service/Container/ContainerInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * ContainerInterface file
+ *
+ * PHP Version 8.1
+ *
+ * @category PHP
+ * @package  OC_P5_BLOG
+ * @author   Mehdi Haddou <mehdih.devapp@gmail.com>
+ * @license  MIT Licence
+ * @link     https://p5.mehdi-haddou.fr
+ */
+
+declare(strict_types=1);
+
+namespace App\Service\Container;
+
+
+/**
+ * Container interface.
+ * Interface for all services
+ *
+ * PHP Version 8.1
+ *
+ * @category PHP
+ * @package  OC_P5_BLOG
+ * @author   Mehdi Haddou <mehdih.devapp@gmail.com>
+ * @license  MIT Licence
+ * @link     https://p5.mehdi-haddou.fr
+ */
+interface ContainerInterface
+{
+
+}
