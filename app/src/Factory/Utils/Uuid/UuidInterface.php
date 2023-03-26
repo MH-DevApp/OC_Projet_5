16f85d054099ee21be8a6111b5cc681eb46c9893
mehdi <mehdih.devapp@gmail.com>
2023-03-26 11:37:06 +0200

    feat (#21) : create tool generate uuidv4

---
+++ app/src/Factory/Utils/Uuid/UuidInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * UuidInterface file.
+ *
+ * PHP Version 8.1
+ *
+ * @category PHP
+ * @package  OC_P5_BLOG
+ * @author   Mehdi Haddou <mehdih.devapp@gmail.com>
+ * @license  MIT Licence
+ * @link     https://p5.mehdi-haddou.fr
+ */
+
+declare(strict_types=1);
+
+namespace App\Factory\Utils\Uuid;
+
+use Exception;
+
+/**
+ * Uuid interface
+ *
+ * PHP Version 8.1
+ *
+ * @category PHP
+ * @package  OC_P5_BLOG
+ * @author   Mehdi Haddou <mehdih.devapp@gmail.com>
+ * @license  MIT Licence
+ * @link     https://p5.mehdi-haddou.fr
+ */
+interface UuidInterface
+{
+
+    static function generate(): string;
+
+}
---
+++ app/src/Factory/Utils/Uuid/UuidV4.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * UuidV4 file
+ *
+ * PHP Version 8.1
+ *
+ * @category PHP
+ * @package  OC_P5_BLOG
+ * @author   Mehdi Haddou <mehdih.devapp@gmail.com>
+ * @license  MIT Licence
+ * @link     https://p5.mehdi-haddou.fr
+ */
+
+declare(strict_types=1);
+
+namespace App\Factory\Utils\Uuid;
+
+use Exception;
+
+/**
+ * UuidV4 class
+ *
+ * Generate a unique string id in format UuidV4 (36 chars)
+ *
+ * @category PHP
+ * @package  OC_P5_BLOG
+ * @author   Mehdi Haddou <mehdih.devapp@gmail.com>
+ * @license  MIT Licence
+ * @link     https://p5.mehdi-haddou.fr
+ */
+class UuidV4 implements UuidInterface
+{
+
+    /**
+     * @throws Exception
+     */
+    public static function generate(): string
+    {
+        $data = random_bytes(16);
+
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+        // Output the unique string id with format UuidV4 (36 chars)
+        return vsprintf(
+            '%s%s-%s-%s-%s-%s%s%s',
+            str_split(bin2hex($data), 4)
+        );
+    }
+
+}
