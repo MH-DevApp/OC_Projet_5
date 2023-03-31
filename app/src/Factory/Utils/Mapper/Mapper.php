176c7099c985b5b5fd8fd9e074f0dbae9dc58d1c
Mehdi H <78570781+MH-DevApp@users.noreply.github.com>
2023-03-31 21:40:48 +0200

    feat : Tool Mapper class (#29) and Tests (#30) (#84)
    * feat : tool Mapper class (#29) :
    - map Entity to array associative ;
    - map array associative to Entity.
    
    * tests (#83) : all cases (#30) of Mapper tool class (#29) :
    - Entity to array associative ;
    - Array associative to Entity;
    - Value null for both functions.

---
+++ app/src/Factory/Utils/Mapper/Mapper.php
@@ -0,0 +1,121 @@
+<?php
+
+/**
+ * Mapper file
+ *
+ * PHP Version 8.1
+ *
+ * @category PHP
+ * @package  OC_P5_BLOG
+ * @author   Mehdi Haddou <mehdih.devapp@gmail.com>
+ * @license  MIT Licence
+ * @link     https://p5.mehdi-haddou.fr
+ */
+
+declare(strict_types=1);
+
+namespace App\Factory\Utils\Mapper;
+
+
+use App\Entity\AbstractEntity;
+use DateTime;
+use ReflectionClass;
+
+/**
+ * Mapper class
+ *
+ * Map an array associative to entity and entity to array
+ * associative.
+ *
+ * @category PHP
+ * @package  OC_P5_BLOG
+ * @author   Mehdi Haddou <mehdih.devapp@gmail.com>
+ * @license  MIT Licence
+ * @link     https://p5.mehdi-haddou.fr
+ */
+abstract class Mapper
+{
+
+
+    /**
+     * Map array associative to entity
+     *
+     * @param array<string, string|int> $obj
+     * @param class-string|string $entityName
+     *
+     * @return object|null
+     */
+    public static function mapArrayToEntity(
+        array $obj,
+        string $entityName
+    ): ?object
+    {
+        if (class_exists($entityName)) {
+            $entity = new $entityName();
+
+            if (!$entity instanceof AbstractEntity) {
+                return null;
+
+            }
+
+            foreach ($obj as $key => $value) {
+                if (property_exists($entity, $key)) {
+                    if ($value) {
+                        $key[0] = strtoupper($key[0]);
+                        $method = "set".$key;
+
+                        if (method_exists($entity::class, $method)) {
+                            $entity->$method($value);
+                        }
+
+                    }
+
+                }
+            }
+
+            return $entity;
+
+        }
+
+        return null;
+
+    }
+
+
+    /**
+     * Map Entity to array associative
+     *
+     * @param object $entity
+     *
+     * @return array<string, string|int>|null
+     */
+    public static function mapEntityToArray(object $entity): ?array
+    {
+        if (class_exists($entity::class) && $entity instanceof AbstractEntity) {
+            $reflexionClass = new ReflectionClass($entity::class);
+            $obj = [];
+
+            foreach ($reflexionClass->getProperties() as $props) {
+                $name = $props->getName();
+                $name[0] = strtoupper($name[0]);
+                $method = "get".$name;
+
+                if ($reflexionClass->hasMethod($method)) {
+                    $obj[$props->getName()] = $entity->$method() instanceof DateTime ?
+                        $entity->$method()->format(DATE_ATOM) :
+                        $entity->$method();
+
+                }
+
+            }
+
+            return $obj;
+
+        }
+
+        return null;
+
+    }
+
+
+}
