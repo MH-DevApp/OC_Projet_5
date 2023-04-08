d4dbd9d77081e10e3c3678f273f67cf65b3c3302
mehdi <mehdih.devapp@gmail.com>
2023-04-08 16:40:39 +0200

    feat : Auth class (#75) - authenticate function : check if credentials are valid ; - isAuthenticated function : chef if user is authenticated

---
+++ app/src/Auth/Auth.php
@@ -0,0 +1,227 @@
+<?php
+
+/**
+ * Auth file
+ *
+ * PHP Version 8.1
+ *
+ * @category PHP
+ * @package  OC_P5_BLOG
+ * @author   Mehdi Haddou <mehdih.devapp@gmail.com>
+ * @license  MIT Licence
+ * @link     https://p5.mehdi-haddou.fr
+ */
+
+declare(strict_types=1);
+
+namespace App\Auth;
+
+
+use App\Entity\Session;
+use App\Entity\User;
+use App\Factory\Manager\Manager;
+use App\Factory\Router\Request;
+use App\Factory\Utils\Csrf\Csrf;
+use App\Repository\SessionRepository;
+use App\Repository\UserRepository;
+use App\Service\Container\Container;
+use App\Service\Container\ContainerInterface;
+use Exception;
+
+/**
+ * Auth class
+ * Make connexion with DB environment app and return
+ * a PDO
+ *
+ * PHP Version 8.1
+ *
+ * @category PHP
+ * @package  OC_P5_BLOG
+ * @author   Mehdi Haddou <mehdih.devapp@gmail.com>
+ * @license  MIT Licence
+ * @link     https://p5.mehdi-haddou.fr
+ */
+class Auth implements ContainerInterface
+{
+
+    public static ?User $currentUser;
+    private Manager $manager;
+    private Request $request;
+    private SessionRepository $sessionRepository;
+
+
+    /**
+     * Constructor
+     *
+     */
+    public function __construct()
+    {
+        self::$currentUser = null;
+
+        /**
+         * @var Manager $manager
+         */
+        $manager = Container::getService("manager");
+
+        /**
+         * @var Request $request
+         */
+        $request = Container::getService("request");
+
+        $this->manager = $manager;
+        $this->request = $request;
+        $this->sessionRepository = new SessionRepository();
+
+    }
+
+
+    /**
+     * Check credentials with the user in database.
+     *
+     * @param array<string, string> $credentials
+     *
+     * @throws Exception
+     */
+    public function authenticate(array $credentials): bool
+    {
+        if ($credentials["email"] && $credentials["password"]) {
+            /**
+             * @var User $user
+             */
+            $user = (new UserRepository())
+                ->findByOne(
+                    ["email" => $credentials["email"]]
+                    , classObject: User::class
+                );
+
+            if (
+                is_object($user) &&
+                password_verify(
+                    $credentials["password"],
+                    $user->getPassword() ?? ""
+                )
+            ) {
+                $this->isAuthenticateSuccessful($user);
+                return true;
+
+            }
+        }
+
+        return false;
+
+    }
+
+
+    /**
+     * Create session in the database and set the cookie session.
+     *
+     * @throws Exception
+     */
+    private function isAuthenticateSuccessful(User $user): void
+    {
+        if (is_string($user->getId())) {
+            // check if old session of user exist, and delete it if true
+            /**
+             * @var Session $oldSession
+             */
+            $oldSession = $this->sessionRepository
+                ->findByOne(
+                    ["userId" => $user->getId()],
+                    classObject: Session::class
+                );
+
+            if (
+                is_object($oldSession)
+            ) {
+                $this->manager->delete($oldSession);
+
+            }
+
+            // Save the new session of user authenticated
+            $session = (new Session())
+                ->setUserId($user->getId());
+            $this->manager->flush($session);
+
+            if (is_string($session->getId())) {
+                // Generate the signature of cookie session
+                $sign = Csrf::generateTokenCsrf($session->getId());
+
+                // Set cookie session with format Json
+                $this->request->setCookie(
+                    "session",
+                    json_encode([
+                        "id" => $session->getId(),
+                        "sign" => $sign
+                    ]) ?: "",
+                    time() + 60 * 60 * 24 * 14
+                );
+
+                self::$currentUser = $user;
+
+            }
+
+        }
+
+    }
+
+
+    /**
+     * Check if user is authenticated
+     *
+     * @return bool
+     */
+    public function isAuthenticated(): bool
+    {
+        // check if cookie session exist
+        /**
+         * @var string $sessionCookieJson
+         */
+        $sessionCookieJson = $this->request->getCookie("session");
+
+        if ($sessionCookieJson) {
+            /**
+             * @var array<string, string> $sessionCookie
+             */
+            $sessionCookie = json_decode($sessionCookieJson, true);
+
+            // Check if signature is valid
+            if (Csrf::isTokenCsrfValid($sessionCookie["sign"], $sessionCookie["id"])) {
+                /**
+                 * @var Session $session
+                 */
+                $session = (new SessionRepository())
+                    ->findByOne(
+                        ["id" => $sessionCookie["id"]],
+                        classObject: Session::class
+                    ) ?? null;
+
+                if (is_object($session) && is_string($session->getUserId())) {
+                    /**
+                     * @var ?User $user
+                     */
+                    $user = (new UserRepository())
+                        ->findByOne(
+                            ["id" => $session->getUserId()],
+                            classObject: User::class
+                        );
+
+                    self::$currentUser = $user;
+
+                }
+
+                return self::$currentUser !== null;
+
+            } else {
+                // Delete cookie session invalid
+                $this->request->setCookie("session", "", time() - 1);
+
+            }
+
+        }
+
+        return false;
+
+    }
+
+
+}
