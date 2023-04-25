911a79b3c5585e9b2f5a0bcbc5b656eaaa21e1b3
mehdi <mehdih.devapp@gmail.com>
2023-04-25 17:19:05 +0200

    feat : Forgotten Password methods (#121) and Forms : - form ask link to reset forgotten password ; - form reset forgotten password ;

--- app/src/Controller/AuthController.php
+++ app/src/Controller/AuthController.php
@@ -20,6 +20,8 @@ namespace App\Controller;
 use App\Auth\Auth;
 use App\Entity\User;
 use App\Factory\Form\ConnexionForm;
+use App\Factory\Form\ForgottenPasswordForm;
+use App\Factory\Form\ForgottenPasswordResetForm;
 use App\Factory\Form\RegisterForm;
 use App\Factory\Mailer\Email;
 use App\Factory\Mailer\Mailer;
@@ -120,6 +122,11 @@ class AuthController extends AbstractController
             $tokenEmail = UuidV4::generate();
 
             /**
+             * @var string $hashTokenEmail
+             */
+            $hashTokenEmail = Csrf::generateTokenCsrf($tokenEmail);
+
+            /**
              * @var string $password
              */
             $password = $user->getPassword();
@@ -131,7 +138,7 @@ class AuthController extends AbstractController
                 )
             );
 
-            $user->setEmailValidateToken(Csrf::generateTokenCsrf($tokenEmail));
+            $user->setEmailValidateToken($hashTokenEmail);
             $user->setExpiredEmailTokenAt();
 
             /**
@@ -146,10 +153,15 @@ class AuthController extends AbstractController
                 true
             );
 
+            /**
+             * @var string $userEmail
+             */
+            $userEmail = $user->getEmail();
+
             // Send email
             $email = (new Email())
                 ->setSubject("[P5] Bienvenue sur P5 DAPS BLOG - Veuillez confirmer votre adresse email")
-                ->setTo($user->getEmail())
+                ->setTo($userEmail)
                 ->setBody(
                     "<p>Bonjour ".$user->getFirstname().", <br><br>".
                         "Je vous souhaite la bienvenue sur P5 DAPS BLOG! <br><br>".
@@ -221,18 +233,21 @@ class AuthController extends AbstractController
     )]
     public function validEmail(string $token): Response
     {
+        /**
+         * @var string $hashToken
+         */
         $hashToken = Csrf::generateTokenCsrf($token);
         $userRepo = new UserRepository();
 
         /**
-         * @var User $user
+         * @var User|false $user
          */
         $user = $userRepo->findByOne(
             ["emailValidateToken" => $hashToken],
             classObject: User::class
         );
 
-        if (!$user || !Csrf::isTokenCsrfValid($user->getEmailValidateToken(), $token)) {
+        if (!$user || !Csrf::isTokenCsrfValid($user->getEmailValidateToken() ?: "", $token)) {
             return $this->responseHttpNotFound();
         }
 
@@ -241,11 +256,16 @@ class AuthController extends AbstractController
          */
         $manager = Container::getService("manager");
 
+        /**
+         * @var string $userEmail
+         */
+        $userEmail = $user->getEmail();
+
         if ($user->getExpiredEmailTokenAt() > new DateTime("now")) {
             // Send email
             $email = (new Email())
-                ->setSubject("[P5] Bienvenue sur P5 DAPS BLOG - Votre compte est activé!")
-                ->setTo($user->getEmail())
+                ->setSubject("[P5] P5 DAPS BLOG - Votre compte est activé!")
+                ->setTo($userEmail)
                 ->setBody(
                     "<p>Bonjour ".$user->getFirstname().", <br><br>".
                     "Je vous souhaite la bienvenue sur P5 DAPS BLOG! Votre adresse email a été confirmée ".
@@ -271,6 +291,10 @@ class AuthController extends AbstractController
         }
 
         $token = UuidV4::generate();
+
+        /**
+         * @var string $hashToken
+         */
         $hashToken = Csrf::generateTokenCsrf($token);
 
         $urlValidEmail = $this->generateUrl(
@@ -285,10 +309,15 @@ class AuthController extends AbstractController
 
         $manager->flush($user);
 
+        /**
+         * @var string $emailUser
+         */
+        $emailUser = $user->getEmail();
+
         // Send email
         $email = (new Email())
-            ->setSubject("[P5] Bienvenue sur P5 DAPS BLOG - Lien de validation de votre email expiré!")
-            ->setTo($user->getEmail())
+            ->setSubject("[P5] P5 DAPS BLOG - Lien de validation de votre email expiré!")
+            ->setTo($emailUser)
             ->setBody(
                 "<p>Bonjour ".$user->getFirstname().", <br><br>".
                 "Le lien de validation de votre adresse email est expiré. <br><br>".
@@ -303,6 +332,207 @@ class AuthController extends AbstractController
         (new Mailer())->send($email);
 
         return $this->render("auth/valid-email-failed.html.twig");
+
+    }
+
+
+    /**
+     * Forgotten password to reset password
+     *
+     * @return Response
+     *
+     * @throws LoaderError|RuntimeError|SyntaxError
+     * @throws RouterException
+     * @throws Exception
+     */
+    #[Route(
+        "/auth/forgotten-password",
+        "app_auth_forgotten_password",
+        methods: ["GET", "POST"])
+    ]
+    public function forgottenPassword(): Response
+    {
+        if (Auth::$currentUser) {
+            return $this->redirectTo("app_home");
+        }
+
+        $form = new ForgottenPasswordForm();
+        $form->handleRequest();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userRepo = new UserRepository();
+
+            /**
+             * @var string $email
+             */
+            $email = $form->getData("email");
+
+            /**
+             * @var User|false $user
+             */
+            $user = $userRepo->findByOne(["email" => $email], classObject: User::class);
+
+            if ($user) {
+                /**
+                 * @var Manager $manager
+                 */
+                $manager = Container::getService("manager");
+
+                $token = UuidV4::generate();
+
+                /**
+                 * @var string $hashToken
+                 */
+                $hashToken = Csrf::generateTokenCsrf($token);
+
+                $link = $this->generateUrl(
+                    "app_auth_forgotten_password_reset",
+                    ["token" => $token],
+                    true
+                );
+
+                $user
+                    ->setForgottenPasswordToken($hashToken)
+                    ->setExpiredTokenAt();
+
+                $manager->flush($user);
+
+                /**
+                 * @var string $emailUser
+                 */
+                $emailUser = $user->getEmail();
+
+                // Send email
+                $emailTemplate = (new Email())
+                    ->setSubject("[P5] P5 DAPS BLOG - Réinitialisation de votre mot de passe")
+                    ->setTo($emailUser)
+                    ->setBody(
+                        "<p>Bonjour ".$user->getFirstname().", <br><br>".
+                        "Nous avons bien reçu votre demande de réinitialisation de mot de passe. <br>".
+                        "Afin de procéder à la réinitialisation, veuillez cliquer sur le lien ci-dessous et ".
+                        "suivre les instructions (Celui-ci est valide pour une durée de 5 minutes) : <br><br>".
+                        "<a href=\"".$link."\">$link</a> <br><br>".
+                        "Si vous n'êtes pas à l'origine de cette demande, veuillez ignorer ce message et votre mot ".
+                        "de passe actuel restera inchangé. <br><br>".
+                        "Si vous avez des questions ou des problèmes avec la réinitialisation de votre mot de passe, ".
+                        "n'hésitez pas me contacter via le formulaire de contact.<br><br>".
+                        "Cordialement, <br>".
+                        "Mehdi"
+                    );
+                (new Mailer())->send($emailTemplate);
+            }
+
+            return $this->render("auth/forgotten-password-send-email.html.twig", [
+                "email" => $email
+            ]);
+
+        }
+
+
+        return $this->render("auth/forgotten-password.html.twig", [
+            "data" => $form->getData(),
+            "errors" => $form->getErrors(),
+            "submitted" => $form->isSubmitted()
+        ]);
+
+    }
+
+
+    /**
+     * Reset password with forgotten password token
+     *
+     * @param string $token
+     *
+     * @return Response
+     *
+     * @throws LoaderError|RuntimeError|SyntaxError
+     * @throws Exception
+     *
+     */
+    #[Route(
+        "/auth/forgotten-password/reset-password/:token",
+        "app_auth_forgotten_password_reset",
+        regexs: ["token" => "(\w){8}((\-){1}(\w){4}){3}(\-){1}(\w){12}"],
+        methods: ["GET", "POST"]
+    )]
+    public function resetForgottenPassword(string $token): Response
+    {
+        /**
+         * @var string $hashToken
+         */
+        $hashToken = Csrf::generateTokenCsrf($token);
+        $userRepo = new UserRepository();
+
+        /**
+         * @var User|false $user
+         */
+        $user = $userRepo->findByOne(
+            ["forgottenPasswordToken" => $hashToken],
+            classObject: User::class
+        );
+
+        if (!$user || !Csrf::isTokenCsrfValid($user->getForgottenPasswordToken() ?: "", $token)) {
+            return $this->responseHttpNotFound();
+
+        }
+
+        if ($user->getExpiredTokenAt() < new DateTime("now")) {
+            return $this->render("auth/forgotten-password-token-expired.html.twig");
+
+        }
+
+        $form = new ForgottenPasswordResetForm();
+        $form->handleRequest();
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /**
+             * @var Manager $manager
+             */
+            $manager = Container::getService("manager");
+
+            /**
+             * @var string $password
+             */
+            $password = $form->getData("password");
+
+            $user
+                ->setPassword(password_hash($password, PASSWORD_ARGON2ID))
+                ->setForgottenPasswordToken(null);
+
+            $manager->flush($user);
+
+            $link = $this->generateUrl("app_auth_login", isAbsolute: true);
+
+            /**
+             * @var string $emailUser
+             */
+            $emailUser = $user->getEmail();
+
+            // Send email
+            $emailTemplate = (new Email())
+                ->setSubject("[P5] P5 DAPS BLOG - Votre mot de passe a été réinitialisé")
+                ->setTo($emailUser)
+                ->setBody(
+                    "<p>Bonjour ".$user->getFirstname().", <br><br>".
+                    "Nous vous confirmons que votre mot de passe a été réinitialisé avec succès. <br>".
+                    "Vous pouvez désormais vous connecter à votre compte : <a href=\"$link\">$link</a>. <br><br>".
+                    "Si vous rencontrez des problèmes pour vous connecter à votre compte ou si vous avez ".
+                    "des questions, n'hésitez pas me contacter via le formulaire de contact.<br><br>".
+                    "Cordialement, <br>".
+                    "Mehdi"
+                );
+            (new Mailer())->send($emailTemplate);
+            
+            return $this->redirectTo("app_auth_login");
+
+        }
+
+
+        return $this->render("auth/forgotten-password-reset.html.twig", [
+            "data" => $form->getData(),
+            "errors" => $form->getErrors(),
+            "submitted" => $form->isSubmitted()
+        ]);
 
     }
 
