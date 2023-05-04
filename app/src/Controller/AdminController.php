8831438073946026f89ce616cfc74a665c5ac8a5
mehdi <mehdih.devapp@gmail.com>
2023-05-04 18:58:25 +0200

    feat (#79) : Add routes in AdminController class for load and manage user entity : - Route for load user dashboard page ; - Route for toggle status user as API call ; - Route for toggle role user as API call

--- app/src/Controller/AdminController.php
+++ app/src/Controller/AdminController.php
@@ -17,9 +17,16 @@ declare(strict_types=1);
 namespace App\Controller;
 
 
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Entity\User;
+use App\Factory\Manager\Manager;
 use App\Factory\Router\Response;
 use App\Factory\Router\Route;
 use App\Factory\Router\RouterException;
+use App\Repository\UserRepository;
+use App\Service\Container\Container;
+use Exception;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
@@ -43,6 +50,19 @@ class AdminController extends AbstractController
         "posts",
         "comments"
     ];
+
+    private Manager $manager;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        /**
+         * @var Manager $manager
+         */
+        $manager = Container::getService("manager");
+        $this->manager = $manager;
+    }
 
 
     /**
@@ -74,6 +94,7 @@ class AdminController extends AbstractController
      * @return Response
      *
      * @throws LoaderError|RuntimeError|SyntaxError
+     * @throws Exception
      */
     #[Route(
         "/admin/dashboard/:page",
@@ -92,6 +113,163 @@ class AdminController extends AbstractController
             "page" => $page
         ]);
 
+    }
+
+    /**
+     * Get entities with name page
+     */
+    #[Route(
+        "/admin/dashboard/entities/:page",
+        "app_admin_entities_page",
+        regexs: ["page" => "(\w)+"],
+        granted: "ROLE_ADMIN"
+    )]
+    public function getEntities(string $page): Response
+    {
+        if (!in_array($page, self::PAGES_DASHBOARD)) {
+            return $this->responseHttpNotFound();
+
+        }
+
+        $entities = [];
+
+        switch ($page) {
+            case "users":
+                $entities = (new UserRepository())->getUsersForDashboard();
+                break;
+            case "posts":
+            case "comments":
+                break;
+        }
+
+        return $this->json([
+            "success" => $entities !== false,
+            "entities" => $entities
+        ]);
+    }
+
+
+    /**
+     * Update status user
+     *
+     * @throws Exception
+     */
+    #[Route(
+        "/admin/user/:id/toggle-status",
+        "admin_user_toggle_status",
+        regexs: ["id" => "(\w){8}((\-){1}(\w){4}){3}(\-){1}(\w){12}"],
+        granted: "ROLE_ADMIN"
+    )]
+    public function toggleStatusUser(string $id): Response
+    {
+        /**
+         * @var User|false $user
+         */
+        $user = (new UserRepository())
+            ->findByOne(
+                ["id" => $id],
+                classObject: User::class
+            );
+
+        if (!$user) {
+            return $this->json(
+                [
+                "success" => false,
+                "message" => "Impossible de modifier le status de cet utilisateur, celui-ci n'a pas été trouvé dans la base de données."
+                ]
+            );
+        }
+
+        if ($user->getId() === $this->user()?->getId()) {
+            return $this->json(
+                [
+                    "success" => false,
+                    "message" => "Impossible de modifier le status de votre compte."
+                ]
+            );
+        }
+
+        $status = $user->getStatus();
+        $user->setStatus(
+            $status === User::STATUS_CODE_REGISTERED_WAITING ||
+            $status === User::STATUS_CODE_DEACTIVATED ?
+                User::STATUS_CODE_REGISTERED :
+                User::STATUS_CODE_DEACTIVATED
+        );
+
+        $this->manager->flush($user);
+
+        return $this->json(
+            [
+                "success" => true,
+                "message" => "Le status de l'utilisateur a été ".
+                    (
+                        $user->getStatus() === User::STATUS_CODE_REGISTERED ?
+                        "activé" :
+                        "désactivé"
+                    ).
+                    " avec succès.",
+                "action" => "update-status"
+            ]
+        );
+    }
+
+
+    /**
+     * Update status user
+     *
+     * @throws Exception
+     */
+    #[Route(
+        "/admin/user/:id/toggle-role",
+        "admin_user_toggle_role",
+        regexs: ["id" => "(\w){8}((\-){1}(\w){4}){3}(\-){1}(\w){12}"],
+        granted: "ROLE_ADMIN"
+    )]
+    public function toggleRoleUser(string $id): Response
+    {
+        /**
+         * @var User|false $user
+         */
+        $user = (new UserRepository())
+            ->findByOne(
+                ["id" => $id],
+                classObject: User::class
+            );
+
+        if (!$user) {
+            return $this->json(
+                [
+                "success" => false,
+                "message" => "Impossible de modifier le role de cet utilisateur, celui-ci n'a pas été trouvé dans la base de données."
+                ]
+            );
+        }
+
+        if ($user->getId() === $this->user()?->getId()) {
+            return $this->json(
+                [
+                    "success" => false,
+                    "message" => "Impossible de modifier le role de votre compte."
+                ]
+            );
+        }
+
+        $user->setRole(
+            $user->getRole() === "ROLE_ADMIN" ?
+                "ROLE_USER" :
+                "ROLE_ADMIN"
+        );
+
+        $this->manager->flush($user);
+
+        return $this->json(
+            [
+                "success" => true,
+                "message" => "Le rôle de l'utilisateur a été modifié avec succès.",
+                "action" => "update-role"
+            ]
+        );
     }
 
 
--- app/src/Repository/UserRepository.php
+++ app/src/Repository/UserRepository.php
@@ -42,11 +42,45 @@ class UserRepository extends AbstractRepository
     /**
      * Construct
      *
-     * @throws RepositoryException
      */
     public function __construct()
     {
         parent::__construct(User::class);
+
+    }
+
+
+    /**
+     *
+     *
+     * @return array<int, User>
+     */
+    public function getUsersForDashboard(): array
+    {
+        $query = "
+            SELECT u.id, u.lastname, u.firstname, u.pseudo, u.email, u.createdAt, u.role, u.status, (SELECT COUNT(*) FROM post WHERE userId = u.id) AS `countPosts`, (SELECT COUNT(*) FROM comment WHERE userId = u.id) as `countComments`
+            FROM `user` as u
+            ORDER BY u.lastname
+        ";
+
+//        $query2 = "
+//            SELECT u.id, u.lastname, u.firstname, u.pseudo, u.email, u.createdAt, u.role, u.status, IFNULL(countPosts, 0), IFNULL(countComments,0)
+//            FROM `user` as u
+//            left join (
+//                SELECT COUNT(*) as countPosts, userId FROM post p  group by userId
+//
+//            ) tmp1 on tmp1.userId = u.id
+//                left join (
+//               SELECT COUNT(*) as countComments, userId FROM comment group by userId
+//
+//            ) tmp2 on tmp2.userId = u.id
+//
+//            ORDER BY u.lastname
+//        ";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute();
+
+        return $statement->fetchAll();
 
     }
 
